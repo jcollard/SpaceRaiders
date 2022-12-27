6554b8c4c8530349120f755e598051cb5ec43b50
Joseph Collard <jcollard@users.noreply.github.com>
2022-12-27 06:49:37 -0700

    refactor: Rework Explosion to use Particle System. Refactored Enemy Ship / Player to use Sound + Explosion. Added Explosion to Asteroids.

--- Space Raiders/Assets/Scripts/ExplosionController.cs
+++ Space Raiders/Assets/Scripts/ExplosionController.cs
@@ -2,17 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(ParticleSystem))]
 public class ExplosionController : MonoBehaviour
 {
-    public void Explode(DestructableController destructable)
-    {
-        ExplosionController e = Instantiate(this, destructable.transform.position, Quaternion.identity);
-        e.gameObject.SetActive(true);
-    }
-
     public void Explode(MonoBehaviour destructable)
     {
         ExplosionController e = Instantiate(this, destructable.transform.position, Quaternion.identity);
+        ParticleSystem particles = e.GetComponent<ParticleSystem>();
         e.gameObject.SetActive(true);
+        e.Invoke(nameof(DestroySelf), particles.main.duration + 1);
     }
+
+    private void DestroySelf() => Destroy(this.gameObject);
 }
